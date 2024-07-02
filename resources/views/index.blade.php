a13b70014ebb2f3e554d80478c591dd23096b914
att124 <daniel.fernandez@davinci.edu.ar>
2024-07-02 14:19:21 -0300

    Cambios navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,57 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/Style.css') }}">
+@include('componentes.navbar')
 
-    <title>Inicio</title>
-
-
-</head>
-<body>
-<header class="ColorPag">
-
-
-    <nav class="navbar navbar-expand-lg bg-pink paddingNavbar">
-        <div class="container-fluid colornav">
-            <a class="nav-link active espaciadoA" aria-current="page" href="#">   <img src="https://fakeimg.pl/300x70/?text=Hello1" alt="Logo del ecommerce" width="300" height="70" class="espaciadoA"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle espaciadoA" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Productos
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Categoria1</a></li>
-                            <li><a class="dropdown-item" href="#">Categoria2</a></li>
-                            <li><a class="dropdown-item" href="#">Categoria3</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link espaciadoB" href="#">Contacto</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link espaciadoA" href="#">Iniciar sesión</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link espaciadoA" href="#">Registrarse</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
-</header>
     <main class="ColorPag">
 
         <div id="bordegris">
