2e74f7420f196656ffbf5a434668166a141c0420
att124 <daniel.fernandez@davinci.edu.ar>
2024-07-02 12:55:29 -0300

    Modelo Usuario

---
+++ app/Models/Usuario.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasFactory;
+
+    protected $table = 'usuario';
+    protected $primaryKey = 'id_usuario';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'usuario',
+        'contrasena',
+        'activo',
+        'fecha_ingreso',
+        'DatosUsuario_idDatosUsuario',
+        'Rol_idRol'
+    ];
+
+    // Relación con el modelo Rol
+    public function rol()
+    {
+        return $this->belongsTo(Rol::class, 'Rol_idRol', 'idRol');
+    }
+
+
+
+
+
+}
