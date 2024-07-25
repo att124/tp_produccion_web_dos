372e60995da16375344c59bd965fcf7710e330eb
att124 <daniel.fernandez@davinci.edu.ar>
2024-07-25 14:30:36 -0300

    Cambios en el hover de los desplegables (Navbar)

--- resources/views/componentes/navbar.blade.php
+++ resources/views/componentes/navbar.blade.php
@@ -7,6 +7,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
@@ -14,9 +15,9 @@
                         <a class="nav-link dropdown-toggle espaciadoA hovernav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Productos
                         </a>
-                        <ul class="dropdown-menu">
+                        <ul class="dropdown-menu bg-pink">
                             @forelse ($categorias as $categoria)
-                            <li><a href="{{route('categorias.mostrar',$categoria->id)}}" class="dropdown-item">{{$categoria->Categoria}}</a></li>
+                            <li><a href="{{route('categorias.mostrar',$categoria->id)}}" class="dropdown-item hoverdropitems">{{$categoria->Categoria}}</a></li>
                             @empty
                             <li class="dropdown-item">No hay categorias</li>
                             @endforelse
@@ -39,15 +40,15 @@
                             {{Auth::user()->name}}
                         </a>
 
-            <ul class="dropdown-menu">
+            <ul class="dropdown-menu bg-pink">
                     <li class="nav-item">
-                        <a href="{{ route('users.show', Auth::user()->id) }}" class="dropdown-item">Perfil </a>
+                        <a href="{{ route('users.show', Auth::user()->id) }}" class="dropdown-item hoverdropitems">Perfil </a>
                     </li>
 
                     <li class="nav-item">
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
-                            <button type="submit" class="dropdown-item">Cerrar sesión</button>
+                            <button type="submit" class="dropdown-item hoverdropitems">Cerrar sesión</button>
                         </form>
                     </li>
             </ul>
@@ -75,12 +76,12 @@
                         <a class="nav-link dropdown-toggle espaciadoA hovernav" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Administradores
                         </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{route('roles.index')}}">Roles</a></li>
-                            <li><a class="dropdown-item" href="{{route('categorias.index')}}">Categorias</a></li>
-                            <li><a class="dropdown-item" href="{{ route('users.index') }}">Usuarios</a></li>
-                            <li><a class="dropdown-item" href="{{route('productos.index')}}">Lista de productos</a></li>
-                            <li><a class="dropdown-item" href="{{route('provincias.index')}}">  Lista de provincias</a></li>
+                        <ul class="dropdown-menu bg-pink">
+                            <li><a class="dropdown-item hoverdropitems" href="{{route('roles.index')}}">Roles</a></li>
+                            <li><a class="dropdown-item hoverdropitems" href="{{route('categorias.index')}}">Categorias</a></li>
+                            <li><a class="dropdown-item hoverdropitems" href="{{ route('users.index') }}">Usuarios</a></li>
+                            <li><a class="dropdown-item hoverdropitems" href="{{route('productos.index')}}">Lista de productos</a></li>
+                            <li><a class="dropdown-item hoverdropitems" href="{{route('provincias.index')}}">  Lista de provincias</a></li>
                         </ul>
                     </li>
                     @endif
